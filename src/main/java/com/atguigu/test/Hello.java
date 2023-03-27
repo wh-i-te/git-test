88e34413da509d598f2dfe18491bcc234cacd143
wh-i-te <2283298341@qq.com>
2023-03-27 18:09:41 +0800

    second commit

--- src/main/java/com/atguigu/test/Hello.java
+++ src/main/java/com/atguigu/test/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git By IDEA");
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
+
     }
 }
